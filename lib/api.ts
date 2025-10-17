ee054008944a73b67f02b418eaa422e0df041fc8
Aye Myint Htet <ayemyinthtet@Ayes-Mac-mini.local>
2025-10-17 14:21:26 +0700

    update

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
+const baseUrl = window.location.origin || 'http://localhost:3000';
 
 export async function fetchApi(
   endpoint: string,
